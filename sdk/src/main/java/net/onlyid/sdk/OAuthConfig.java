ab21e0c9eed5bc5e81fcc1e8b42ef0a93078d808
ltb <liangtb@qq.com>
2020-09-23 20:52:13 +0800

    refactor: 规范文案大小写

--- sdk/src/main/java/net/onlyid/sdk/OAuthConfig.java
+++ sdk/src/main/java/net/onlyid/sdk/OAuthConfig.java
@@ -3,7 +3,7 @@ package net.onlyid.sdk;
 import java.io.Serializable;
 
 public class OAuthConfig implements Serializable {
-    public String clientId; // 你的应用id
+    public String clientId; // 你的应用ID
     public String view; // 显示界面，设置为zoomed 放大显示，否则默认正常显示
     public String theme; // 主题样式，设置为dark 夜间主题，否则默认日间主题
     public String state; // oauth安全相关，不懂可以忽略
