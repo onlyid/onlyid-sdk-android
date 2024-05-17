864547ca7cd9afbf2faa428d37e09ffe1e2792f9
ltb <liangtb@qq.com>
2024-05-17 16:36:54 +0800

    refactor: 优化调用sdk的demo部分。

--- app/src/main/java/net/onlyid/demo/MainActivity.java
+++ app/src/main/java/net/onlyid/demo/MainActivity.java
@@ -1,33 +1,32 @@
-package net.onlyid.sdk_test;
+package net.onlyid.demo;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-import net.onlyid.sdk.OAuthConfig;
 import net.onlyid.sdk.OnlyID;
 
 public class MainActivity extends Activity {
     static final String TAG = "OnlyID";
     static final int REQUEST_OAUTH = 1;
 
+    TextView resultTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button test = findViewById(R.id.test);
-        test.setOnClickListener((View v) -> {
-            // prd
-            String clientId = "0958a5d2a9614ae2813397c1f3bc6b19";
-            // dev
-//            String clientId = "047f9fff2c2647529c7e5c69b1f40b0d";
+        Button loginButton = findViewById(R.id.login_button);
+        resultTextView = findViewById(R.id.result_text_view);
 
-            OAuthConfig config = new OAuthConfig(clientId);
-            OnlyID.oauth(this, config, REQUEST_OAUTH);
+        loginButton.setOnClickListener((View v) -> {
+            String clientId = "73c6cce568d34a25ac426a26a1ca0c1e"; // prd 运行demo的开发者使用这个
+//            String clientId = "3c9dfdd977434ab2aff330d4d20c3955"; // dev 内部开发使用，请忽略
+            OnlyID.startOAuth(this, REQUEST_OAUTH, clientId);
         });
     }
 
@@ -39,12 +38,10 @@ public class MainActivity extends Activity {
 
         if (resultCode == RESULT_OK) {
             String code = data.getStringExtra(OnlyID.EXTRA_CODE);
-            Log.d(TAG, "登录成功，code= " + code);
+            resultTextView.setText("登录成功，code= " + code);
+            // 得到code后，使用code换取用户信息的逻辑三端一致（Web、Android、iOS），详见官网文档
         } else if (resultCode == RESULT_CANCELED) {
-            Log.d(TAG, "用户取消（拒绝）");
-        } else if (resultCode == OnlyID.RESULT_ERROR) {
-            Exception exception = (Exception) data.getSerializableExtra(OnlyID.EXTRA_EXCEPTION);
-            Log.w(TAG, "发生错误", exception);
+            resultTextView.setText("用户取消");
         }
     }
 }
